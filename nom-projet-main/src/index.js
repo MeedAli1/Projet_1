98f05ff736c1a4bc0d1dd3cfe79947f208ed4955
MeedAli1 <167976408+MeedAli1@users.noreply.github.com>
2025-04-25 21:47:01 +0100

    2

--- nom-projet-main/src/index.js
+++ nom-projet-main/src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './i18n';
 import './Styles/Chat.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
